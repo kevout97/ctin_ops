30aa37dca266041ea9230677e0c39d7eabbb744c
kevout <kev.gomez97@hotmail.com>
2019-02-25 15:34:59 -0600

    Archivo PHP para obtener el json sin insertar nada a la base de datos

--- Php/plugin-insert.php
+++ Php/plugin-insert.php
@@ -2,7 +2,7 @@
     require_once("DataBase.php");
     require_once("ApiRequest.php");
     require_once("ApiResponse.php");
-    if(isset($_POST["ticket_id"]) && isset($_POST["id_owner"]) && isset($_POST["id_cause"])){
+    if(isset(isset($_POST["id_owner"]) && isset($_POST["id_cause"])){
         $db = new DataBase("mysql57","root","3KfIhp3UDC","api");/* Modificar estos datos */
         $apiRequest = new ApiRequest(filter_var($_POST["id_owner"],FILTER_SANITIZE_NUMBER_INT),filter_var($_POST["ticket_id"],FILTER_SANITIZE_NUMBER_INT),filter_var($_POST["ticket_number"],FILTER_SANITIZE_NUMBER_INT),filter_var($_POST["id_queues"],FILTER_SANITIZE_NUMBER_INT),filter_var($_POST["comments"],FILTER_SANITIZE_STRING),filter_var($_POST["id_cause"],FILTER_SANITIZE_NUMBER_INT));
         $query = "INSERT INTO incidents (ID_OWNER,ID_QUEUE,ID_CAUSE,DATE,TICKET_NUM,TICKET_ID,COMMENTS) VALUES (". $apiRequest->getIdOwner() .",". $apiRequest->getIdQueues() ."," . $apiRequest->getIdCause() .", now(),". $apiRequest->getTicketNumber() .",". $apiRequest->getTicketId() .",'". $apiRequest->getComments() ."')";
