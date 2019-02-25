a551291c6f15b0987c64f0ab0163cf244001ae81
Mauricio Mp <mauricio.melendez@outlook.com>
2019-02-24 19:33:20 -0600

    val

--- Front/val.js
+++ Front/val.js
@@ -33,15 +33,37 @@ function valida(f) {
   }
 
   obj1 = {"owners":[{"ID_OWNER":"2","NAME":"Jose Villa"},{"ID_OWNER":"3","NAME":"Metztli Alonso"},{"ID_OWNER":"4","NAME":"Eduardo Sanchez"},{"ID_OWNER":"5","NAME":"Axl Lopez"},{"ID_OWNER":"6","NAME":"Oscar Meza"},{"ID_OWNER":"7","NAME":"Magali Lopez"}],"causes":[{"ID_CAUSE":"1","CAUSE":"Capacitacion"},{"ID_CAUSE":"2","CAUSE":"Documentacion"},{"ID_CAUSE":"3","CAUSE":"Error humano"}],"queues":[]}
-  html = "";
+  own = "";
   for(var key in obj1.owners) {
-      html += "<option value=" + obj1.owners[key].ID_OWNER  + ">" +obj1.owners[key].NAME + "</option>"
+      own += "<option value=" + obj1.owners[key].ID_OWNER  + ">" +obj1.owners[key].NAME + "</option>"
       
   }
-  document.getElementById("id_Owner").innerHTML = html;
+  document.getElementById("id_Owner").innerHTML = own;
   document.getElementById("id_Owner").selectedIndex = -1;
 
+  cause = "";
+  for(var key in obj1.causes) {
+      cause += "<option value=" + obj1.causes[key].ID_CAUSE  + ">" +obj1.causes[key].CAUSE + "</option>"
+      
+  }
+  document.getElementById("cause").innerHTML = cause;
+  document.getElementById("cause").selectedIndex = -1;
 
+  que = "";
+  for(var key in obj1.queues) {
+      que += "<option value=" + obj1.owners[key].ID_OWNER  + ">" +obj1.owners[key].NAME + "</option>"
+      
+  }
+  document.getElementById("queues").innerHTML = que;
+  document.getElementById("queues").selectedIndex = -1;
 
+  window.addEventListener("load", function() {
+    Form.tick.addEventListener("keypress", soloNumeros, false);
+  });
 
-  
+  function soloNumeros(e){
+    var key = window.event ? e.which : e.keyCode;
+    if (key < 48 || key > 57) {
+      e.preventDefault();
+    }
+  }
